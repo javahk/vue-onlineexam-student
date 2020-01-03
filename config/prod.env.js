d6e7778aefd8e4a8d2ca3e5bc74dbb29358cad3e
rui.wang <rui.wang@shangri-la.com>
2020-01-03 18:15:30 +0800

    prod.env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://maweitao.top/oes/api/student"',
-  BASE_WEBSOCKET: '"ws://maweitao.top:8080/oes/api/websocket/"'
+  BASE_API: '"http://51ctofx.com/oes/api/student"',
+  BASE_WEBSOCKET: '"ws://51ctofx.com:8080/oes/api/websocket/"'
 }
